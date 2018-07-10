0769e9785ebb12ad7c30784405cb833ef1dbda6d
davidpodhola <david@podhola.net>
2018-07-10 12:32:51 +0200

    exec format

--- idempiere_micro.sh
+++ idempiere_micro.sh
@@ -1,5 +1,7 @@
-    wget https://github.com/iDempiere-micro/idempiere-micro/blob/master/runtime/etc/idempiere.properties && \
-    mv idempiere.properties /opt/karaf/etc && \
-    wget https://github.com/iDempiere-micro/idempiere-micro/releases/download/v${IDEMPIERE_MICRO_VERSION}/v${IDEMPIERE_MICRO_VERSION}.tgz && \
-    tar -C /opt/karaf/deploy -xzf v${IDEMPIERE_MICRO_VERSION}.tgz && \
-    rm v${IDEMPIERE_MICRO_VERSION}.tgz
+#!/bin/sh
+
+wget https://github.com/iDempiere-micro/idempiere-micro/blob/master/runtime/etc/idempiere.properties && \
+mv idempiere.properties /opt/karaf/etc && \
+wget https://github.com/iDempiere-micro/idempiere-micro/releases/download/v${IDEMPIERE_MICRO_VERSION}/v${IDEMPIERE_MICRO_VERSION}.tgz && \
+tar -C /opt/karaf/deploy -xzf v${IDEMPIERE_MICRO_VERSION}.tgz && \
+rm v${IDEMPIERE_MICRO_VERSION}.tgz
