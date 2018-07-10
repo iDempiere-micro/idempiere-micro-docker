b4dea244cc9167a3f8b18d06ea2fb358f625bc5d
davidpodhola <david@podhola.net>
2018-07-10 19:28:03 +0200

    standalone script to run iDempiere micro in Docker

---
+++ src/run_idempiere_micro.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+old_id="$(docker ps -aq --filter name=idempiere-micro-karaf)"
+
+docker stop $old_id
+docker rm $old_id
+
+sudo docker volume create --name idempiere-micro-karaf-data
+sudo docker volume create --name idempiere-micro-karaf-deploy
+sudo docker volume create --name idempiere-micro-karaf-etc
+
+docker run --rm --name idempiere-micro-karaf -d \
+  --link idempiere-pgsql:idempiere-db \
+  -p 1099:1099 -p 8101:8101 -p 44444:44444 -p 8181:8181 \
+  -v idempiere-micro-karaf-deploy:/opt/karaf/deploy -v idempiere-micro-karaf-data:/opt/karaf/data -v idempiere-micro-karaf-etc:/opt/karaf/etc \
+  naseukolycz/idempiere-micro:latest
+
+sleep 10
+docker exec -it idempiere-micro-karaf /tmp/setup_idempiere_micro_karaf.sh
+sleep 10
+docker exec -it idempiere-micro-karaf /opt/karaf/bin/client "system:shutdown -f -r"
