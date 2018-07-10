27399ed83455a5d5732019764c632f39f304faa9
davidpodhola <david@podhola.net>
2018-07-10 12:40:25 +0200

    wget -> curl

--- idempiere_micro.sh
+++ idempiere_micro.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-wget https://github.com/iDempiere-micro/idempiere-micro/blob/master/runtime/etc/idempiere.properties && \
+curl -s -o "idempiere.properties" "https://github.com/iDempiere-micro/idempiere-micro/blob/master/runtime/etc/idempiere.properties" && \
 mv idempiere.properties /opt/karaf/etc && \
-wget https://github.com/iDempiere-micro/idempiere-micro/releases/download/v${IDEMPIERE_MICRO_VERSION}/v${IDEMPIERE_MICRO_VERSION}.tgz && \
+curl -s -o "v${IDEMPIERE_MICRO_VERSION}.tgz" "https://github.com/iDempiere-micro/idempiere-micro/releases/download/v${IDEMPIERE_MICRO_VERSION}/v${IDEMPIERE_MICRO_VERSION}.tgz" && \
 tar -C /opt/karaf/deploy -xzf v${IDEMPIERE_MICRO_VERSION}.tgz && \
 rm v${IDEMPIERE_MICRO_VERSION}.tgz
--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
-old_id="$(docker ps -aq --filter name=idempiere-micro-karaf)"
-
-docker stop $(old_id)
-docker rm $(old_id)
+#!/bin/sh
+./stop.sh
 docker run --rm --name idempiere-micro-karaf -d -p 1099:1099 -p 8101:8101 -p 44444:44444 -p 8181:8181 naseukolycz/idempiere-micro:latest
 sleep 10
 docker exec -it idempiere-micro-karaf /opt/karaf/bin/client -r 7  "feature:install http; feature:install http-whiteboard; feature:install war; feature:install webconsole"
