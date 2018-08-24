59283757fe782293192a4eef8080605c885a59e5
davidpodhola <david@podhola.net>
2018-08-24 21:30:36 +0200

    test moved to a script for better local testing

---
+++ test.sh
@@ -0,0 +1,12 @@
+cd src
+docker build --no-cache -t naseukolycz/idempiere-micro:test .
+docker run --name idempiere-micro-karaf-test -p 8181:8181 -d naseukolycz/idempiere-micro:test
+sleep 10
+docker exec -i idempiere-micro-karaf-test /tmp/setup_idempiere_micro_karaf.sh
+sleep 10
+docker exec -i idempiere-micro-karaf-test /opt/karaf/bin/client "system:shutdown -f -r"
+sleep 10
+wget "http://localhost:8181/idempiere/api/status"
+str="works!"
+value=`cat status`
+if [[ "$value" != "$str" ]]; then exit 1; fi
