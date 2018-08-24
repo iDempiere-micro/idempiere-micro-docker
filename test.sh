48a4adf54a2b1f52714d7e71fcca2415e0f8acec
davidpodhola <david@podhola.net>
2018-08-24 21:47:52 +0200

    build image in the correct version

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 cd src
-docker build --no-cache -t naseukolycz/idempiere-micro:test .
-docker run --name idempiere-micro-karaf-test -p 8181:8181 -d naseukolycz/idempiere-micro:test
+docker build --no-cache -t naseukolycz/idempiere-micro:1.0.0 .
+docker run --name idempiere-micro-karaf-test -p 8181:8181 -d naseukolycz/idempiere-micro:1.0.0
 sleep 10
 docker exec -i idempiere-micro-karaf-test /tmp/setup_idempiere_micro_karaf.sh
 sleep 10
