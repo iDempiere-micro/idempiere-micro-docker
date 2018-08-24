aeffd62cb392e7bad267ab4c92d55304f6d2d568
davidpodhola <david@podhola.net>
2018-08-24 21:35:18 +0200

    fixing the new tag name (1.0.0 instead of v1.0.0)

--- src/idempiere_micro.sh
+++ src/idempiere_micro.sh
@@ -5,5 +5,5 @@ rm -rf /opt/karaf/etc/idempiere.properties && \
 sed -i -- 's/localhost/idempiere-db/g' /tmp/idempiere.properties
 sed -i -- 's/5433/5432/g' /tmp/idempiere.properties
 cp /tmp/idempiere.properties /opt/karaf/etc/ && \
-curl -L -s -o "/tmp/idempire-micro.tgz" "https://github.com/iDempiere-micro/idempiere-micro/releases/download/v${IDEMPIERE_MICRO_VERSION}/v${IDEMPIERE_MICRO_VERSION}.tgz" && \
+curl -L -s -o "/tmp/idempire-micro.tgz" "https://github.com/iDempiere-micro/idempiere-micro/releases/download/${IDEMPIERE_MICRO_VERSION}/v${IDEMPIERE_MICRO_VERSION}.tgz" && \
 tar -C /opt/karaf/deploy -xzf /tmp/idempire-micro.tgz
