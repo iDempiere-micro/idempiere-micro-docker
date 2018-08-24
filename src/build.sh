0d18bee29375697c16e0f833789c0e3d95e4fbc9
davidpodhola <david@podhola.net>
2018-08-24 21:27:18 +0200

    upgrade to v1.0.0

--- src/build.sh
+++ src/build.sh
@@ -1,2 +1,2 @@
 ./stop.sh
-docker build --no-cache -t naseukolycz/idempiere-micro:0.0.1 -t naseukolycz/idempiere-micro:latest .
+docker build --no-cache -t naseukolycz/idempiere-micro:1.0.0 -t naseukolycz/idempiere-micro:latest .
